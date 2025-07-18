a2e6032992a929c42c2e38fb99057e4d94800b1d
Mizael_web <mizael55411006@edum.rn.senac.br>
2025-07-18 08:53:52 -0300

    feat  atividade gestao_bike  rodando na porta 3001, criado banco no PgAdmin e testado todas a rotas no insominia, tudo funcionando 100%

---
+++ src/modulos/categoria/models/categoria.model.js
@@ -0,0 +1,44 @@
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../../../config/configDb');
+
+const CategoriaModel = sequelize.define('Categoria', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: 'O nome da categoria é obrigatório.'
+      },
+      len: {
+        args: [3, 100],
+        msg: 'O nome da categoria deve ter entre 3 e 100 caracteres.'
+      }
+    }
+  },
+  descricao: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        msg: 'A descrição é obrigatória.'
+      },
+      len: {
+        args: [10, 255],
+        msg: 'A descrição deve ter entre 10 e 255 caracteres.'
+      }
+    }
+  }
+}, {
+  tableName: 'categoria',
+  createdAt: 'criado_em',
+  updatedAt: 'atualizado_em',
+  underscored: true, // Para compatibilidade com snake_case
+});
+
+module.exports = CategoriaModel;
